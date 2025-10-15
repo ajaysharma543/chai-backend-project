aa8be8517d83908e13374566cd29d2f133c8cbb3
Ajay Sharma <sharmaa8203@gmail.com>
2025-10-15 10:09:31 +0530

    make piplines and sub piplines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ import {asyncHandler} from '../utils/asynchandler.js'
 import { Uploadincloudnary } from '../utils/cloudinary.js';
 import {v2 as cloudinary} from "cloudinary"
 import { deleteFromCloudinary } from "../utils/deletefromcloudinart.js";
-import { Aggregate } from "mongoose";
+import mongoose from "mongoose";
 
 const generateAccessAndRefereshTokens  = async (userId) => {
     try {
@@ -323,7 +323,7 @@ const {username} = req.params;
         throw new ApiError(400, "username is missing")
     }
 
-    const Channel = await User.Aggregate([
+    const Channel = await User.aggregate([
         {
             $match : {
                 username: username?.toLowerCase()
@@ -394,6 +394,60 @@ const {username} = req.params;
 
 })
 
+const getWatchHistory = asyncHandler(async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
+})
+
 export {
     registerUser,
     loginUser,
@@ -404,5 +458,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory,
     }
